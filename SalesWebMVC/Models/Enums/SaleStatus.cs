f1b9f8390f457f2443e185e002d344876977a600
G4ma <renatomedeiros2001@gmail.com>
2021-01-08 12:12:26 +0000

    Seeding Service

--- SalesWebMVC/Models/Enums/SaleStatus.cs
+++ SalesWebMVC/Models/Enums/SaleStatus.cs
@@ -4,6 +4,6 @@
     {
         Pending = 0,
         Billed = 1,
-        Canceld = 2
+        Canceled = 2
     }
 }
--- SalesWebMVC/Startup.cs
+++ SalesWebMVC/Startup.cs
@@ -38,14 +38,17 @@ namespace SalesWebMVC
 
             services.AddDbContext<SalesWebMVCContext>(options =>
                     options.UseSqlServer(Configuration.GetConnectionString("SalesWebMVCContext")));
+
+            services.AddScoped<SeedingService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, SeedingService seedingService)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                seedingService.Seed();
             }
             else
             {
